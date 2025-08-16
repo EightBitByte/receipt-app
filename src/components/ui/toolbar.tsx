c6d0488e710dbc573878045d5ee2d06f54d2d92d
Jacob Moy <apixelatedpug@gmail.com>
2025-08-16 15:42:49 -0700

    :sparkles: Add shad/cn button to toolbar

--- src/components/ui/toolbar.tsx
+++ src/components/ui/toolbar.tsx
@@ -1,14 +1,17 @@
 import { UserRound } from "lucide-react";
+import { Button } from "./button";
 
 export default function Toolbar() {
   return (
-    <div className="flex flex-row justify-between px-4 pt-4">
+    <div className="flex flex-row items-center justify-between px-4 pt-4">
       <h1>Receipt App</h1>
       <div className="md:flex flex-row gap-4 hidden">
         <h2>Login</h2>
         <h2>Sign Up</h2>
       </div>
-      <UserRound className="md:hidden"/>
+      <Button variant="outline" size="icon" className="md:hidden">
+        <UserRound />
+      </Button>
     </div>
   )
 }
